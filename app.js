af29b3e81d656d7ef6fe1b9644902d4225f9b8b0
Yoshi-purple <n7ydahew@gmail.com>
2020-12-07 17:03:12 +0900

    クォーテーション変更しました。

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /** @format */
 
-"use strict";
+'use strict';
 
 {
-  const answer = "東京";
-  while (prompt("日本の首都は？") !== answer) {
-    alert("不正解です！");
+  const answer = '東京';
+  while (prompt('日本の首都は？') !== answer) {
+    alert('不正解です！');
   }
-  alert("正解です！");
+  alert('正解です！');
 }
